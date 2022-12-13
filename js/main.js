d6e5c29815ed1bd3e3a06193337a6e496c99e288
Abdalla-Rabeh <abdallahcodeing@gmail.com>
2022-12-13 13:02:17 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let link = document.querySelectorAll("nav li a");
+
+link.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    link.forEach((li) => {
+      li.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+  });
+});
+
+$('nav .dropdown').hover(function(){
+  var $this = $(this);
+  $this.addClass('show');
+  $this.find('> a').attr('aria-expanded', true);
+  $this.find('.dropdown-menu').addClass('show');
+}, function(){
+  var $this = $(this);
+    $this.removeClass('show');
+    $this.find('> a').attr('aria-expanded', false);
+    $this.find('.dropdown-menu').removeClass('show');
+});
