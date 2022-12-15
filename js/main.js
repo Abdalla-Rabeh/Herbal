8f82d45dc0629643a0d0f93a1d4fcf03506453f8
Abdalla-Rabeh <abdallahcodeing@gmail.com>
2022-12-16 00:19:51 +0200

    updata

--- js/main.js
+++ js/main.js
@@ -24,3 +24,71 @@ $('nav .dropdown').hover(function(){
 $(document).ready(function () {
   $(".loader").fadeOut(2000);
 });
+new WOW().init();
+var scroll_top =$('.Scroll-top')
+$(window).scroll(function(){
+    if($(window).scrollTop() >= 100){
+        if(scroll_top.is(':hidden')){
+        $(scroll_top).fadeIn(400)
+        }
+    }else{
+        $(scroll_top).fadeOut(400)
+    }
+})
+$(".scroll_top").click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop:0
+        
+    },1000)
+})
+
+//  Fun Loading WebSite
+Loading = () =>{
+  window.addEventListener("load", () => {
+      let Loader = document.querySelector(".Loader");
+      setTimeout(() => {
+      Loader.classList.add('Loader-none');
+      }, 3000);
+  });
+  }
+  Loading() 
+
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 1,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+    },
+  });
